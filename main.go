86f069e438f816e784a5ae1db05d18a6873305c3
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2019-11-03 21:30:22 +0200

    Add bytes feature and appropriate tests

--- main.go
+++ main.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
+// ErrInvalidFlags raises an error if multiple flags are set
+// where you'd want only one, example - print line and word count
+var ErrInvalidFlags = errors.New("invalid flags or combination of supplied")
+
 func main() {
 	lines := flag.Bool("l", false, "count lines")
+	bytes := flag.Bool("b", false, "count bytes")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(l io.Reader, countLines bool) int {
+func count(l io.Reader, countLines bool, countBytes bool) (int, error) {
 	b := bufio.NewScanner(l)
-	if !countLines {
-		b.Split(bufio.ScanWords)
+
+	if countLines && countBytes {
+		return 0, ErrInvalidFlags
 	}
+	if !countLines && !countBytes {
+		b.Split(bufio.ScanWords)
+	} else if !countLines {
+		b.Split(bufio.ScanBytes)
+	}
+
 	wc := 0
 	for b.Scan() {
 		wc++
 	}
-	return wc
+	return wc, nil
 }
--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("one two three four\n")
 	expected := 4
-	actual := count(b, false)
+	actual, _ := count(b, false, false)
 	if actual != expected {
 		t.Errorf("Expected %d, got %d instead\n", expected, actual)
 	}
@@ -17,8 +17,25 @@ func TestCountWords(t *testing.T) {
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("one two three\nfour five six\nsix six six\n")
 	expected := 3
-	actual := count(b, true)
+	actual, _ := count(b, true, false)
 	if actual != expected {
 		t.Errorf("Expected %d, got %d instead\n", expected, actual)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("one two three\n")
+	expected := 14
+	actual, _ := count(b, false, true)
+	if actual != expected {
+		t.Errorf("Expected %d, got %d instead\n", expected, actual)
+	}
+}
+
+func TestCountInvalidArgs(t *testing.T) {
+	b := bytes.NewBufferString("failing test\n")
+	_, err := count(b, true, true)
+	if err == nil {
+		t.Errorf("Expected an error, received nil\n")
 	}
 }
