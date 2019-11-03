10be53281e815615339df0905694109d8caa1317
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2019-11-03 20:33:23 +0200

    word count supports count by lines

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	lines := flag.Bool("l", false, "count lines")
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(l io.Reader) int {
+func count(l io.Reader, countLines bool) int {
 	b := bufio.NewScanner(l)
-	b.Split(bufio.ScanWords)
+	if !countLines {
+		b.Split(bufio.ScanWords)
+	}
 	wc := 0
 	for b.Scan() {
 		wc++
--- main_test.go
+++ main_test.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"testing"
 	"bytes"
+	"testing"
 )
 
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("one two three four\n")
 	expected := 4
-	actual := count(b)
+	actual := count(b, false)
+	if actual != expected {
+		t.Errorf("Expected %d, got %d instead\n", expected, actual)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("one two three\nfour five six\nsix six six\n")
+	expected := 3
+	actual := count(b, true)
 	if actual != expected {
 		t.Errorf("Expected %d, got %d instead\n", expected, actual)
 	}
